3ba9ddbdb72ee31cb5bf64fb8439c773b48e994f
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-10-27 22:41:56 +0100

    Adds vagrant chef base files

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name              'vagrant-cakephp-box'
+maintainer        'ALT3 B.V.'
+license           'MIT'
+description       'Chef provisioned Vagrant Cakephp box'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '0.0.1'
+
+supports 'ubuntu'
