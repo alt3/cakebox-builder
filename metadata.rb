26b18658c6feccb272688556cc1dc2505c7fc5a4
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-04 22:17:58 +0100

    Bumps cb version

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer        'ALT3 B.V.'
 license           'MIT'
 description       'Chef provisioned Vagrant Cakephp box'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.0.1'
+version           '0.0.2'
 
 supports 'ubuntu'
